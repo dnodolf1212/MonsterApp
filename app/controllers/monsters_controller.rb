1bdb137900dc576a67c25346105f0e76be3c05d5
dnodolf1212 <dnodolf@gmail.com>
2021-05-22 20:14:06 -0700

    full CRUD functionality for Monster model. Use partials in place of full form for new and edit pages. Use set_monster helper method in before_action for monsters_controller to reduce code smell.

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,12 +1,42 @@
 class MonstersController < ApplicationController
- 
+  before_action :set_monster, only: [:show, :edit, :update, :destroy]
+
   def index
     @monsters = Monster.all
     render 'monsters/index' #this is optional!!
   end
 
-  def show 
+  def show ;end
+
+  def new 
+    @monster = Monster.new
+  end
+
+  def create 
+    monster = Monster.create(monster_params)
+    redirect_to monster_path(monster)
+  end
+
+  def edit;end
+
+  def update 
+    @monster.update(monster_params)
+    redirect_to monster_path(@monster)
+  end
+
+  def destroy
+    @monster.destroy
+    redirect_to monsters_path
+  end
+
+  private 
+
+  def set_monster 
     @monster = Monster.find_by_id(params[:id])
+  end
+
+  def monster_params
+    params.require(:monster).permit(:name, :scare_factor, :hunger)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/monsters', to: 'monsters#index'
-  get '/monsters/:id', to: 'monsters#show', as: 'monster'
+  #get '/monsters', to: 'monsters#index'
+  #get '/monsters/:id', to: 'monsters#show', as: 'monster'
+  resources :monsters
 end

440e470a771e0105947d29e80eb93ae30ba776cd
dnodolf1212 <dnodolf@gmail.com>
2021-05-27 00:21:56 -0700

    add validations on monster model. Include error handling on new view with messages.

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,4 +1,5 @@
 class Monster < ApplicationRecord
-  validates :name, presence: true
-
+  validates :name, presence: {message: ":You must name the Beast."} 
+  validates :hunger, presence: true
+  validates :scare_factor, inclusion: 1..100 
 end
